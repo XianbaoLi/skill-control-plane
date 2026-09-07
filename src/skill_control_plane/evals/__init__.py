967588c63ed0153b61e449c53581aa09a86169fc
李先宝 <lixianbao_2022@qq.com>
2026-09-08 00:37:28 +0800

    eval: export runtime retrieval evaluator

--- src/skill_control_plane/evals/__init__.py
+++ src/skill_control_plane/evals/__init__.py
@@ -1,3 +1,15 @@
 from .metrics import hard_negative_hit_rate, required_recall_at_k, target_skill_accuracy
+from .retrieval import (
+    RuntimeRetrievalGoldCase,
+    evaluate_union_retrieval,
+    load_runtime_retrieval_gold,
+)
 
-__all__ = ["hard_negative_hit_rate", "required_recall_at_k", "target_skill_accuracy"]
+__all__ = [
+    "RuntimeRetrievalGoldCase",
+    "evaluate_union_retrieval",
+    "hard_negative_hit_rate",
+    "load_runtime_retrieval_gold",
+    "required_recall_at_k",
+    "target_skill_accuracy",
+]
